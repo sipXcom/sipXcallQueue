a3864696ea9f2270bf245c187c2a8db315c7e436
Mircea Carasel <mirceac@ezuce.com>
2021-01-27 08:17:58 -0600

    UC-4863: API to manage Call Queue

---
+++ config-plugin/src/org/sipfoundry/sipxconfig/callqueue/api/CallQueueAgentList.java
@@ -0,0 +1,37 @@
+package org.sipfoundry.sipxconfig.callqueue.api;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.sipfoundry.sipxconfig.callqueue.CallQueueAgent;
+
+@XmlRootElement(name = "agents")
+public class CallQueueAgentList {
+    private List<CallQueueAgentBean> m_callQueueAgents;
+
+    public void setCallQueueAgents(List<CallQueueAgentBean> callQueueAgents) {
+        m_callQueueAgents = callQueueAgents;
+    }
+
+    @XmlElement(name = "queue")
+    public List<CallQueueAgentBean> getCallQueueAgents() {
+        if (m_callQueueAgents == null) {
+            m_callQueueAgents = new ArrayList<CallQueueAgentBean>();
+        }
+        return m_callQueueAgents;
+    }
+
+    public static CallQueueAgentList convertCallQueueAgentList(Collection<CallQueueAgent> callQueueAgentList) {
+        List<CallQueueAgentBean> callQueueAgents = new ArrayList<CallQueueAgentBean>();
+        for (CallQueueAgent callQueueAgent : callQueueAgentList) {
+            callQueueAgents.add(CallQueueAgentBean.convertQueue(callQueueAgent));
+        }
+        CallQueueAgentList list = new CallQueueAgentList();
+        list.setCallQueueAgents(callQueueAgents);
+        return list;
+    }
+}
